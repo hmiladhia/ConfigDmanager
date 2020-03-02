844c5b7987d8f2c39d1ee03d653d72e5d0599a4c
Dhia Hmila <dhiahmila@gmail.com>
2020-03-02 17:15:37 +0100

    - packaged configDmanager

---
+++ demo/demo.py
@@ -0,0 +1,20 @@
+from configDmanager import import_config
+
+
+class RandomClass:
+    def __init__(self, param1, param2, user_info):
+        print(f"param1: {param1}")
+        print(f"param2: {param2}")
+        print(f'my user: {user_info.user}')
+        print(f'my user: {user_info.password}')
+
+
+config = import_config('MainConfig')
+
+print("## Object 1")
+obj = RandomClass(**config)
+
+
+# You can also select specific keys
+print("## Object 2")
+another_obj = RandomClass(param2='Another Value', **config[['param1', 'user_info']])
