b24f00de376148fea1b08317c7932b759fa31cd1
Dhia Hmila <dhiahmila@gmail.com>
2020-03-03 16:23:49 +0100

    automatic setup

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ import setuptools
 
 from configDmanager import import_config, ConfigManager
 
-conf = import_config('PackageConfigs.VersionConfig')
+test = os.environ.get('Test', 'True')
+
+conf_name = 'TestVersionConfig' if test == 'True' else 'VersionConfig'
+conf = import_config(f'PackageConfigs.{conf_name}')
 
 try:
     setuptools.setup(**conf)
@@ -11,4 +14,4 @@ finally:
     gversion, version = conf.version.rsplit('.', 1)
     version = int(version) + 1
     conf.version = f"{gversion}.{version}"
-    ConfigManager.export_config_file(conf, 'VersionConfig', os.path.join(os.getcwd(), 'PackageConfigs'))
+    ConfigManager.export_config_file(conf, conf_name, os.path.join(os.getcwd(), 'PackageConfigs'))
