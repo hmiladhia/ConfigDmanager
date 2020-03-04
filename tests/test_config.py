cd6d88359cbbf4e9135de0253f3fc6cf6fbe5320
Dhia Hmila <dhiahmila@gmail.com>
2020-03-04 12:34:45 +0100

    - added fixtures to tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,28 +4,38 @@ from configDmanager import import_config
 from configDmanager.errors import ReinterpretationError
 
 
-conf = import_config('configs.TestConfig')
-fstring_conf = import_config('configs.FstringConfig')
-yaml_conf = import_config('configs.YamlConfig')
+@pytest.fixture
+def conf():
+    return import_config('configs.TestConfig')
 
 
-def test_param_as_attribute():
+@pytest.fixture
+def fstring_conf():
+    return import_config('configs.FstringConfig')
+
+
+@pytest.fixture
+def yaml_conf():
+    return import_config('configs.YamlConfig')
+
+
+def test_param_as_attribute(conf):
     assert conf.mail_port == 587
 
 
-def test_params_in_parent():
+def test_params_in_parent(conf):
     assert conf.mail_use_tls
 
 
-def test_sub_config():
+def test_sub_config(conf):
     assert conf.user_info.user == "RandomName"
 
 
-def test_params_as_keys():
+def test_params_as_keys(conf):
     assert conf['mail_port'] == 587
 
 
-def test_sub_config_as_keys():
+def test_sub_config_as_keys(conf):
     assert conf['user_info.user'] == "RandomName"
 
 
@@ -33,12 +43,12 @@ def get_params(**kwargs):
     return str(kwargs)
 
 
-def test_dict_as_key():
+def test_dict_as_key(conf):
     expected = {"{'mail_server': 'smtp.google.com', 'tls': True}", "{'tls': True, 'mail_server': 'smtp.google.com'}"}
     assert get_params(**conf[{'mail_server': 'mail_server', 'mail_use_tls': 'tls'}]) in expected
 
 
-def test_iterable_as_key():
+def test_iterable_as_key(conf):
     expected = {"{'mail_server': 'smtp.google.com', 'mail_use_tls': True}",
                 "{'mail_use_tls': True, 'mail_server': 'smtp.google.com'}"}
     assert get_params(**conf[{'mail_server', 'mail_use_tls'}]) in expected
@@ -46,7 +56,7 @@ def test_iterable_as_key():
     assert get_params(**conf[('mail_server', 'mail_use_tls')]) in expected
 
 
-def test_param_add_as_attr():
+def test_param_add_as_attr(conf):
     with pytest.raises(AttributeError):
         conf.param1
     conf.param1 = 'value1'
@@ -54,7 +64,7 @@ def test_param_add_as_attr():
     assert conf['param1'] == "value1"
 
 
-def test_param_add_as_key():
+def test_param_add_as_key(conf):
     with pytest.raises(AttributeError):
         conf.param2
     conf['param2'] = 'value2'
@@ -62,7 +72,7 @@ def test_param_add_as_key():
     assert conf['param2'] == "value2"
 
 
-def test_private_param_add_as_attr():
+def test_private_param_add_as_attr(conf):
     with pytest.raises(AttributeError):
         result = conf.__param1
     conf.__param1 = 'value1'
@@ -74,7 +84,7 @@ def test_private_param_add_as_attr():
     assert'__param1' in conf.to_dict()
 
 
-def test_private_param_add_as_key():
+def test_private_param_add_as_key(conf):
     conf['__param2'] = 'value2'
     with pytest.raises(AttributeError):
         result = conf.__param2
@@ -84,19 +94,19 @@ def test_private_param_add_as_key():
     assert'__param2' in conf.to_dict()
 
 
-def test_real_private_not_in_dict():
+def test_real_private_not_in_dict(conf):
     assert '__config_dict' not in conf.to_dict()
 
 
-def test_fstrings_works():
+def test_fstrings_works(fstring_conf):
     assert fstring_conf['mail'] == 'RandomName@gmail.com'
 
 
-def test_environ_var_works():
+def test_environ_var_works(fstring_conf):
     assert fstring_conf['password'] == '123456'
 
 
-def test_fstrings_recursion_error():
+def test_fstrings_recursion_error(fstring_conf):
     with pytest.raises(ReinterpretationError) as context:
         result = fstring_conf.value1
     expected = {'Param (value1: {value2}) reinterpretation failed: Due to cycle - RecursionError',
@@ -104,31 +114,31 @@ def test_fstrings_recursion_error():
     assert str(context.value) in expected
 
 
-def test_key_access_error_message():
+def test_key_access_error_message(fstring_conf):
     with pytest.raises(KeyError) as context:
         fstring_conf['po']
     assert str(context.value) == "\"Could not find param 'po' in FstringConfig\""
 
 
-def test_environ_access_error_message():
+def test_environ_access_error_message(fstring_conf):
     with pytest.raises(ReinterpretationError) as context:
         fstring_conf['mypassword']
     assert str(context.value) == "Param (mypassword: {os_environ[password]}) reinterpretation failed: Could not find 'password' in Environment variables"
 
 
-def test_reinterpretation_key_access_error_message():
+def test_reinterpretation_key_access_error_message(fstring_conf):
     with pytest.raises(ReinterpretationError) as context:
         fstring_conf['my_other_password']
     assert str(context.value) == "Param (my_other_password: {passwor}) reinterpretation failed: Could not find param 'passwor' in FstringConfig"
 
 
-def test_attribute_access_error_message():
+def test_attribute_access_error_message(fstring_conf):
     with pytest.raises(AttributeError) as context:
         fstring_conf.po
     assert str(context.value) == "'Config' object has no attribute 'po'"
 
 
-def test_file_integration_path_relative_to_cwd():
+def test_file_integration_path_relative_to_cwd(fstring_conf):
     result = fstring_conf.text2
     assert result.startswith("Cum saepe multa, tum memini domi in hemicyclio sedentem,")
     with open('long_text.txt', 'r') as file:
@@ -136,7 +146,7 @@ def test_file_integration_path_relative_to_cwd():
     assert result == expected
 
 
-def test_file_integration_path_relative_to_config():
+def test_file_integration_path_relative_to_config(fstring_conf):
     result = fstring_conf.text
     assert result.startswith("Cum saepe multa, tum memini domi in hemicyclio sedentem,")
     with open('long_text.txt', 'r') as file:
@@ -144,11 +154,11 @@ def test_file_integration_path_relative_to_config():
     assert result == expected
 
 
-def test_file_integration_error():
+def test_file_integration_error(fstring_conf):
     with pytest.raises(ReinterpretationError) as context:
         fstring_conf.text3
     assert str(context.value) == "Param (text3: {read_file[missing.txt]}) reinterpretation failed: [Errno 2] No such file or directory: 'missing.txt'"
 
 
-def test_yaml_subclass():
+def test_yaml_subclass(yaml_conf):
     assert yaml_conf.subconfig.param1 == 'value1'
