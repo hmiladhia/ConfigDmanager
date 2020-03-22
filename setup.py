9a4019025270f997748e05ce03deaafb7bdd8b32
Dhia Hmila <dhiahmila@gmail.com>
2020-03-22 03:27:27 +0100

    setup update

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 import os
 import setuptools
 
-from configDmanager import import_config, ConfigManager
+from configDmanager import import_config, update_config
 
 test = os.environ.get('Test', 'True') == 'True'
 
 conf = import_config('PackageConfigs.VersionConfig')
 
-try:
-    setuptools.setup(**conf)
-except Exception as e:
-    print(e)
-else:
-    if not test:
-        conf['__version.__patch'] += 1
-        ConfigManager.export_config_file(conf, 'VersionConfig', os.path.join(os.getcwd(), 'PackageConfigs'))
+setuptools.setup(**conf)
+
+update_config(lambda cnf: {'__version.__patch': cnf['__version.__patch'] + 1 if not test else 0},
+              'PackageConfigs.VersionConfig')
